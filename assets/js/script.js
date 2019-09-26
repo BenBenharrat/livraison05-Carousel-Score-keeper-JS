a569d34a462f41fa0f819cc59a66c6e5a29e8fc6
BenBenharrat <benjaminbenharrat@gmail.com>
2019-09-26 16:50:14 +0200

    declared empty functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
 let button = document.querySelector('button');
 let li = document.querySelectorAll('li');
 
+function nextPicture() {
+
+}
+
+function checkActiveClass() {
+
+}
+function removeActiveClass() {
+
+}
+function addActiveClass() {
+
+}
 
