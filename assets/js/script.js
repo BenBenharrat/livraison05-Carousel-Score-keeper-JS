ccd52afe03e0d257f5532c2579650fced4e5a95a
BenBenharrat <benjaminbenharrat@gmail.com>
2019-09-27 11:18:02 +0200

    fill function content

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,16 @@ function checkActiveClass() {
 
 }
 function removeActiveClass() {
-
+    li[getCurrentLiIndex()].classList.remove('active')
 }
 function addActiveClass() {
-
+    li[getCurrentLiIndex()].classList.add('active')
 }
 function getCurrentLiIndex() {
-
+    return currentIndex
 }
 function getNextLiIndex() {
-
+    if(getCurrentLiIndex() == li.length - 1 ) return 0;
+    return getCurrentLiIndex() + 1;
 }
 
