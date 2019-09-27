38ad1e872ddef97a54c27902568fdbea77a0e07d
BenBenharrat <benjaminbenharrat@gmail.com>
2019-09-27 14:55:03 +0200

    add function content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,37 @@
 let button = document.querySelector('button');
 let li = document.querySelectorAll('li');
+let currentLiIndex = 0;
 
 function nextPicture() {
-
+    removeActiveClass();
+    currentLiIndex++
+    getNextLiIndex()
+    addActiveClass();
 }
-function checkActiveClass() {
 
-}
 function removeActiveClass() {
-    li[getCurrentLiIndex()].classList.remove('active')
+    li[currentLiIndex].classList.remove('active')
 }
+
 function addActiveClass() {
-    li[getNextLiIndex()].classList.add('active')
+    li[currentLiIndex].classList.add('active')
 }
+
+function getNextLiIndex() {
+    if (currentLiIndex >= li.length ) {
+        currentLiIndex =0;
+    }
+}
+
+document.addEventListener('click', nextPicture);
+
+
+
+
+
+
+/* 
 function getCurrentLiIndex() {
     let arr = Array.from(li);
     return arr.findIndex(item => item.className == 'active');
-}
-function getNextLiIndex() {
-    if(getCurrentLiIndex() == li.length - 1 ) return 0;
-    return getCurrentLiIndex() + 1;
-}
+} */
