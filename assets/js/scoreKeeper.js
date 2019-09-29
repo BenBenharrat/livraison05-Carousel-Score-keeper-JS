4a68f1c73c647514ccc1cecb9928d77e41249965
BenBenharrat <benjaminbenharrat@gmail.com>
2019-09-29 08:50:24 +0200

    declare const

---
+++ assets/js/scoreKeeper.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const h1 = document.querySelector('h1');
+const scorePlayer1 = document.querySelectorAll('span')[0];
+const scorePlayer2 = document.querySelectorAll('span')[1];
+const winnerScore = document.querySelectorAll('span')[2];
+const input = document.querySelector('input');
+const player1Button = document.querySelectorAll('button')[0];
+const player2Button = document.querySelectorAll('button')[1];
+const reset = document.querySelectorAll('button')[2];
+
