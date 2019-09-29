a3d43714e590676e0194a8cb4f78c14b0eaa926b
BenBenharrat <benjaminbenharrat@gmail.com>
2019-09-29 15:11:43 +0200

    add winner score & reset functions

--- assets/js/scoreKeeper.js
+++ assets/js/scoreKeeper.js
@@ -1,24 +1,24 @@
 'use strict'
 
-const h1 = document.querySelector('h1');
-const scorePlayer1 = document.querySelectorAll('span')[0];
-const scorePlayer2 = document.querySelectorAll('span')[1];
-const winnerScore = document.querySelectorAll('span')[2];
-const input = document.querySelector('input');
-const player1Button = document.querySelectorAll('button')[0];
-const player2Button = document.querySelectorAll('button')[1];
-const reset = document.querySelectorAll('button')[2];
+const h1 = document.querySelector('h1') ;
+const scorePlayer1 = document.querySelectorAll('span')[0] ;
+const scorePlayer2 = document.querySelectorAll('span')[1] ;
+const winnerScore = document.querySelectorAll('span')[2] ;
+const input = document.querySelector('input') ;
+const player1Button = document.querySelectorAll('button')[0] ;
+const player2Button = document.querySelectorAll('button')[1] ;
+const reset = document.querySelectorAll('button')[2] ;
 
-let scoreP1 = 0;
-let scoreP2 = 0; 
-let scoreMax = 5;
+let scoreP1 = 0 ;
+let scoreP2 = 0 ; 
+let scoreMax = 5 ;
 
-// === INITIAL ===
+// === INITIAL ==================================
 
 function scoreKeeper() {
-    insertScoreInSpanMax();
-    insertScoreInSpanP1();
-    insertScoreInSpanP2();
+    insertScoreInSpanMax() ;
+    insertScoreInSpanP1() ;
+    insertScoreInSpanP2() ;
 }
 
 function setScoreMax() {
@@ -27,7 +27,7 @@ function setScoreMax() {
 }
 
 
-// === PLAYER 1 ===
+// === PLAYER 1 ==================================
 
 function insertScoreInSpanP1() {
     scorePlayer1.append(document.createTextNode(scoreP1)) ;
@@ -40,11 +40,11 @@ function modifyScoreInSpanP1() {
 function incrementScore1() {
     if (scoreP1 < scoreMax && scoreP2 < scoreMax) {
         scoreP1++ ;
-        scorePlayer1.innerText = scoreP1
+        scorePlayer1.innerText = scoreP1 ;
     }
 }
 
-// === PLAYER 2 ===
+// === PLAYER 2 ==================================
 
 function insertScoreInSpanP2() {
     scorePlayer2.append(document.createTextNode(scoreP2)) ;
@@ -61,22 +61,25 @@ function incrementScore2() {
     }
 }
 
-// === WINNER SCORE ===
+// === WINNER SCORE ===============================
 
 function insertScoreInSpanMax() {
-
+    winnerScore.append(document.createTextNode(scoreMax)) ;
 }
 
 function modifyWinnerScore() {
-
+    winnerScore.innerText = scoreMax ;
 }
 
 function setWinnerScore() {
-
+    insertScoreInSpanMax() ; 
 }
 
-// === RESET ===
+// === RESET ======================================
 
 function resetScores() {
-
+    scoreP1 = 0 ;
+    scoreP2 = 0 ;
+    modifyScoreInSpanP1() ;
+    modifyScoreInSpanP2() ;
 }
